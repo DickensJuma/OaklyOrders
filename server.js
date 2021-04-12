1c3561f4f95678f06d05f10d7dcef5bc4f6ff38e
DickensJuma <dickensjuma13@gmail.com>
2021-04-12 22:42:27 +0300

    feature/1: mongodb connect

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+mongoose.connect(
+    "mongodb+srv://dickens:ugPUWKvrnAuiTs8@cluster0.yeyah.mongodb.net/Oaklydb?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    }
+  );
+  
+
+
+app.listen(3000, () => {
+    console.log("Server is running...");
+  });
